2d380c030eec5e7b5e8ac1f4e52ed72c29207d0e
Leo Generali <leogen17@gmail.com>
2019-08-15 17:38:12 -0400

    Add guestbook

---
+++ src/_data/guestbook.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  const data = [
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' },
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' },
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' },
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' },
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' },
+    { message: 'All your base are belong to us!', author: 'Cool Guy 12' }
+  ];
+
+  return data;
+};
